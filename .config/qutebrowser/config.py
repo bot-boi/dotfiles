6ff7f108961582879c658e0f7785d3752b160419
bot-boi <bot-boi@gmail.com>
2021-02-23 16:43:44 -0500

    Add qutebrowser config
    	* removed some unnecessary files

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,1 +1,16 @@
 config.load_autoconfig(True)
+
+c.auto_save.session = True
+
+c.content.autoplay = False
+c.content.blocking.method = "both"
+
+c.editor.command = ["vim", "{}"]
+
+c.tabs.last_close = "startpage"
+c.tabs.show = "switching"
+
+config.bind(",m", "spawn umpv {url}")
+config.bind(",zl", "spawn --userscript qute-pass")
+config.bind(",zul", "spawn --userscript qute-pass --username-only")
+config.bind(",zpl", "spawn --userscript qute-pass --password-only")
