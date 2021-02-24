a9204aed0d8b17e6893edc6752ef10899aa1bb81
bot-boi <bot-boi@gmail.com>
2021-02-24 03:47:38 -0500

    setup qutebrowser pass integration

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,16 +1,25 @@
-config.load_autoconfig(True)
+config = config
+c = c
+config.load_autoconfig(False)
 
 c.auto_save.session = True
 
 c.content.autoplay = False
 c.content.blocking.method = "both"
 
-c.editor.command = ["vim", "{}"]
+c.editor.command = ["st", "-e", "vim", "{}"]
 
 c.tabs.last_close = "startpage"
+c.tabs.mode_on_change = "persist"
+c.tabs.position = "left"
+c.tabs.select_on_remove = "last-used"
 c.tabs.show = "switching"
+c.tabs.show_switching_delay = 3000
+c.tabs.title.format = "{id}:{current_title}"
+c.tabs.width = 300
 
 config.bind(",m", "spawn umpv {url}")
-config.bind(",zl", "spawn --userscript qute-pass")
-config.bind(",zul", "spawn --userscript qute-pass --username-only")
-config.bind(",zpl", "spawn --userscript qute-pass --password-only")
+config.bind(",M", "hint links spawn umpv {hint-url}")
+config.bind(",zl", "spawn --userscript qute-pass -u '^login: (.*)' -U secret")
+config.bind(",zul", "spawn --userscript qute-pass -u '^login: (.*)' -U secret --username-only -m")
+config.bind(",zpl", "spawn --userscript qute-pass -u '^login: (.*)' -U secret --password-only -m")
